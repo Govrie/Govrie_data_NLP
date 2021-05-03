a67aa0a680560c4473790d6839f0ce673b467eaa
Govrie <55189059+Govrie@users.noreply.github.com>
2021-05-03 23:01:58 +0900

    Update code.py

--- code.py
+++ code.py
@@ -6,3 +6,128 @@ import pandas as pd
 import numpy as np
 import re
 import matplotlib.pyplot as plt
+
+# Twitter Api Credentials
+consumerKey = "wuUNfWouFtimj5l5PUk0uYcMu"
+consumerSecret = "we9LySp1FKVY9QBcPjlf9W2PL4abUS7M1tW44KkIO3m2ebFkuy"
+accessToken = "168671451-qpOiCpxSlxwGzHhn4afsQELIPcP4p8p2thL6k0xL"
+accessTokenSecret = "9CINzaz7MAq8XXxZQ49gksmCr8ttnTCG0cWaYgQ6MJBUd"
+
+# Create the authentication object
+authenticate = tweepy.OAuthHandler(consumerKey, consumerSecret) 
+# Set the access token and access token secret
+authenticate.set_access_token(accessToken, accessTokenSecret) 
+# Creating the API object while passing in auth information
+api = tweepy.API(authenticate, wait_on_rate_limit = True)
+
+# Extract 100 tweets from the twitter user
+posts = api.user_timeline(screen_name="BillGates", count = 100, lang ="en", tweet_mode="extended")
+#  Print the last 5 tweets
+print("Show the 5 recent tweets:\n")
+i=1
+for tweet in posts[:5]:
+    print(str(i) +') '+ tweet.full_text + '\n')
+    i= i+1
+    
+# Create a dataframe with a column called Tweets
+df = pd.DataFrame([tweet.full_text for tweet in posts], columns=['Tweets'])
+# Show the first 5 rows of data
+df.head()
+
+# Create a function to clean the tweets
+def cleanTxt(text):
+ text = re.sub('@[A-Za-z0–9]+', '', text) #Removing @mentions
+ text = re.sub('#', '', text) # Removing '#' hash tag
+ text = re.sub('RT[\s]+', '', text) # Removing RT
+ text = re.sub('https?:\/\/\S+', '', text) # Removing hyperlink
+ 
+ return text
+
+# Clean the tweets
+df['Tweets'] = df['Tweets'].apply(cleanTxt)
+
+# Show the cleaned tweets
+df
+
+# Create a function to get the subjectivity
+def getSubjectivity(text):
+   return TextBlob(text).sentiment.subjectivity
+
+# Create a function to get the polarity
+def getPolarity(text):
+   return  TextBlob(text).sentiment.polarity
+
+
+# Create two new columns 'Subjectivity' & 'Polarity'
+df['Subjectivity'] = df['Tweets'].apply(getSubjectivity)
+df['Polarity'] = df['Tweets'].apply(getPolarity)
+
+# Show the new dataframe with columns 'Subjectivity' & 'Polarity'
+df
+
+# Create a function to compute negative (-1), neutral (0) and positive (+1) analysis
+def getAnalysis(score):
+    if score < 0:
+      return 'Negative'
+    elif score == 0:
+      return 'Neutral'
+    else:
+      return 'Positive'
+
+df['Analysis'] = df['Polarity'].apply(getAnalysis)
+    # Show the dataframe
+df
+
+# Printing positive tweets 
+print('Printing positive tweets:\n')
+j=1
+sortedDF = df.sort_values(by=['Polarity']) #Sort the tweets
+for i in range(0, sortedDF.shape[0] ):
+  if (sortedDF['Analysis'][i] == 'Positive'):
+    print(str(j) + ') '+ sortedDF['Tweets'][i])
+    print()
+    j= j+1
+    
+# Printing negative tweets  
+print('Printing negative tweets:\n')
+j=1
+sortedDF = df.sort_values(by=['Polarity'],ascending=False) #Sort the tweets
+for i in range(0, sortedDF.shape[0] ):
+  if( sortedDF['Analysis'][i] == 'Negative'):
+    print(str(j) + ') '+sortedDF['Tweets'][i] + str(sortedDF['Polarity'][i]))
+    print()
+    j=j+1
+    
+# Plotting 
+plt.figure(figsize=(8,6)) 
+for i in range(0, df.shape[0]):
+  plt.scatter(df["Polarity"][i], df["Subjectivity"][i], color='Blue') 
+# plt.scatter(x,y,color)   
+plt.title('Sentiment Analysis') 
+plt.xlabel('Polarity') 
+plt.ylabel('Subjectivity') 
+plt.show()
+
+# Print the percentage of positive tweets
+ptweets = df[df.Analysis == 'Positive']
+ptweets = ptweets['Tweets']
+ptweets
+
+round( (ptweets.shape[0] / df.shape[0]) * 100 , 1)
+
+# Print the percentage of negative tweets
+ntweets = df[df.Analysis == 'Negative']
+ntweets = ntweets['Tweets']
+ntweets
+
+round( (ntweets.shape[0] / df.shape[0]) * 100, 1)
+
+# Show the value counts
+df['Analysis'].value_counts()
+
+# Plotting and visualizing the counts
+plt.title('Sentiment Analysis')
+plt.xlabel('Sentiment')
+plt.ylabel('Counts')
+df['Analysis'].value_counts().plot(kind = 'bar')
+plt.show()
